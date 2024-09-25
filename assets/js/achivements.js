d047dfd88321f4e8625646afaf63cdc0ca485608
Omar Mammadli <158719141+memmedliomer@users.noreply.github.com>
2024-09-26 00:22:49 +0400

    Update achivements.js

--- assets/js/achivements.js
+++ assets/js/achivements.js
@@ -8,19 +8,14 @@ const projects = [
   {
     title: "SafeOrbitNet\nMobil Application",
     cardImage: "assets/images/project-page/1.png",
-    description: "A quiz app built using basic react.",
     tagimg: "https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png",
-    Previewlink: "",
-    Githublink: "",
+    
   },
   {
     title: "Line Follower Robot ",
     cardImage: "assets/images/project-page/2.png",
-    description: "Built using Python.",
     tagimg:
       "https://camo.githubusercontent.com/888e388801f947dec7c3d843942c277af25fe2b1aed1821542c4e711f210312a/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f7468756d622f632f63332f507974686f6e2d6c6f676f2d6e6f746578742e7376672f37363870782d507974686f6e2d6c6f676f2d6e6f746578742e7376672e706e67",
-    Previewlink: "",
-    Githublink: "",
   },
 ];
 
