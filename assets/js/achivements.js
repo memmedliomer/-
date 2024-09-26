6102531ad2f3742632282a334d3f550f6c91a54f
Omar Mammadli <158719141+memmedliomer@users.noreply.github.com>
2024-09-26 16:36:41 +0400

    Update achivements.js

--- assets/js/achivements.js
+++ assets/js/achivements.js
@@ -3,48 +3,78 @@ AOS.init();
 
 const projectcards = document.querySelector(".projectcards");
 
-// Array of object for projects
+// Array of objects for projects
 const projects = [
   {
     title: "SafeOrbitNet\nMobil Application",
     cardImage: "assets/images/project-page/1.png",
-    
-    
   },
   {
-    title: "Line Follower Robot ",
+    title: "Line Follower Robot",
     cardImage: "assets/images/project-page/2.png",
-    tagimg:
-      "https://camo.githubusercontent.com/888e388801f947dec7c3d843942c277af25fe2b1aed1821542c4e711f210312a/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f7468756d622f632f63332f507974686f6e2d6c6f676f2d6e6f746578742e7376672f37363870782d507974686f6e2d6c6f676f2d6e6f746578742e7376672e706e67",
+  },
+  {
+    title: "Third Project",
+    cardImage: "assets/images/project-page/3.jpg", // Üçüncü resim
+  },
+  {
+    title: "Fourth Project",
+    cardImage: "assets/images/project-page/4.jpg", // Dördüncü resim
+  },
+  {
+    title: "Fifth Project",
+    cardImage: "assets/images/project-page/5.jpg", // Beşinci resim
   },
 ];
 
-// function for rendering project cards data
+// Function for rendering project cards data
 const showCards = () => {
   let output = "";
-  projects.forEach(({ title, cardImage}) => {
+  // İlk iki projeyi göster
+  projects.slice(0, 2).forEach(({ title, cardImage }) => {
     output += `       
-        <div class="column skill-card card" style="margin: 15px"data-aos="zoom-in-up" data-aos-easing="linear" data-aos-delay="300" data-aos-duration="600" >
+        <div class="column skill-card card" style="margin: 15px" data-aos="zoom-in-up" data-aos-easing="linear" data-aos-delay="300" data-aos-duration="600" >
           <div class="wrapper" style="background: url(${cardImage}) center / cover no-repeat;">
-            <div class="header">
-            </div>
+            <div class="header"></div>
             <div class="data">
               <div class="content">
-              <div class="title-div">
-                <h1 class="title"><a href="#">${title}</a></h1>
+                <div class="title-div">
+                  <h1 class="title"><a href="#">${title}</a></h1>
                 </div>
-            <ul class="menu-content"><br>
-                 </ul>
-              </div>
-              <div class="year">
-                <p style="text-align: center; font-size: 20px; color: white;">2025</p>
+                <ul class="menu-content"><br></ul>
               </div>
             </div>
           </div>
         </div>`;
   });
+
+  // 2025 yazısını ekle (İki resimden sonra)
+  output += `
+    <div style="text-align: center; margin-top: 50px;">
+      <h1 style="font-size: 36px; color: black;">2025</h1>
+    </div>`;
+
+  // Üçüncü ve diğer projeleri göster (2025'ten sonra)
+  projects.slice(2).forEach(({ title, cardImage }) => {
+    output += `       
+        <div class="column skill-card card" style="margin: 15px" data-aos="zoom-in-up" data-aos-easing="linear" data-aos-delay="300" data-aos-duration="600" >
+          <div class="wrapper" style="background: url(${cardImage}) center / cover no-repeat;">
+            <div class="header"></div>
+            <div class="data">
+              <div class="content">
+                <div class="title-div">
+                  <h1 class="title"><a href="#">${title}</a></h1>
+                </div>
+                <ul class="menu-content"><br></ul>
+              </div>
+            </div>
+          </div>
+        </div>`;
+  });
+
   projectcards.innerHTML = output;
 };
+
 document.addEventListener("DOMContentLoaded", showCards);
 
 function myFunction() {
