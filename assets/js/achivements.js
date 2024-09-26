e1428049f8d8dabb534a75721dd58d00c04cb81d
Omar Mammadli <158719141+memmedliomer@users.noreply.github.com>
2024-09-26 18:09:06 +0400

    Update achivements.js

--- assets/js/achivements.js
+++ assets/js/achivements.js
@@ -50,7 +50,7 @@ const showCards = () => {
 
     projects.forEach(({ title, cardImage }) => {
       output += `       
-        <div class="column skill-card card" style="margin: 15px" data-aos="zoom-in-up" data-aos-easing="linear" data-aos-delay="300" data-aos-duration="600">
+       
           <div class="wrapper" style="background: url(${cardImage}) center / cover no-repeat;">
             <div class="header"></div>
             <div class="data">
@@ -87,7 +87,7 @@ function myFunction() {
   var input, button, i, skillcard, card, title;
   input = document.getElementById("myInput").value;
   input = input.toUpperCase();
-  skillcard = document.getElementsByClassName("skill-card");
+  //skillcard = document.getElementsByClassName("skill-card");
   card = document.getElementsByClassName("card");
   title = document.getElementsByClassName("title");
 
